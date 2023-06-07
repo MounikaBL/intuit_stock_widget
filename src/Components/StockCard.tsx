c67a05af6418e3a830cef0b059e71ab9ff7eabd9
Jayappa <Rajesh.Jayappa@poly.com>
2023-06-08 00:06:28 +0530

    quick fix

--- src/Components/StockCard.tsx
+++ src/Components/StockCard.tsx
@@ -11,35 +11,38 @@ const StockCard = observer(() => {
     const { stocksStore } = useStore();
     const company: any = stocksStore.displayedStock;
     const timeSeriesData = stocksStore.timeSeriesData;
+    let currPrice: number = parseFloat(get(timeSeriesData, `[0][close]`, "")) > parseFloat(company["52WeekHigh"]) ?
+        parseFloat(company["52WeekHigh"]) : parseFloat(get(timeSeriesData, `[0][close]`, ""));
     return (
         <ErrorBoundary>
-            {Object.keys(company).length ? <div className="stock__card__content">
-                <p className="stock__card__title">{company.Name + " : " + company.Symbol}</p>
-                <p>About Company: {company.Description}</p>
-                <div className="slide__container">
-                    <div className='week__high__low'>
-                        <div>52 weeks low</div>
-                        <div>52 weeks high</div>
+            {Object.keys(company).length ?
+                <div className="stock__card__content">
+                    <p className="stock__card__title">{company.Name + " : " + company.Symbol}</p>
+                    <p>About Company: {company.Description}</p>
+                    <div className="slide__container">
+                        <div className='week__high__low'>
+                            <div>52 weeks low</div>
+                            <div>52 weeks high</div>
+                        </div>
+                        <input type="range"
+                            min={parseFloat(company["52WeekLow"])}
+                            max={parseFloat(company["52WeekHigh"])}
+                            title={"Current price: " + currPrice}
+                            value={currPrice}
+                            className="slider"
+                            disabled
+                        />
+                        <div className='week__high__low'>
+                            <div>{company["52WeekLow"]}</div>
+                            <div>{company["52WeekHigh"]}</div>
+                        </div>
                     </div>
-                    <input type="range"
-                        min={parseFloat(company["52WeekLow"])}
-                        max={parseFloat(company["52WeekHigh"])}
-                        title={"Current price: " + get(timeSeriesData, `[0][close]`, "")}
-                        value={get(timeSeriesData, `[0][close]`, "")}
-                        className="slider"
-                        disabled
-                    />
-                    <div className='week__high__low'>
-                        <div>{company["52WeekLow"]}</div>
-                        <div>{company["52WeekHigh"]}</div>
-                    </div>
-                </div>
-                <p>Current price: {get(timeSeriesData, `[0][close]`, "")}</p>
-                <p>Exchange: {company.Exchange}</p>
-                <p>Industry: {company.Industry}</p>
-                <p>PERatio: {company.PERatio}</p>
-                <p>MarketCapitalization: {company.MarketCapitalization}</p>
-            </div> : <div className='no__data'>No data available</div>}
+                    <p>Current price: {currPrice}</p>
+                    <p>Exchange: {company.Exchange}</p>
+                    <p>Industry: {company.Industry}</p>
+                    <p>PERatio: {company.PERatio}</p>
+                    <p>MarketCapitalization: {company.MarketCapitalization}</p>
+                </div> : <div className='no__data'>No data available</div>}
         </ErrorBoundary>
     );
 });
